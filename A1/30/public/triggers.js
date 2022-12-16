f8855ce9499d865ebba61226711aa26e6512671c
smarterGerman <65780492+smarterGerman@users.noreply.github.com>
2022-12-16 11:33:21 -0500

    Add files via upload

---
+++ A1/30/public/triggers.js
@@ -0,0 +1,14 @@
+let allTriggerAnswersJSON = `[
+{"lernen" : ["Wir müssen reden.", "Ich habe aber keine Zeit. Ich muss lernen.", "Immer musst du lernen.", "Das stimmt doch gar nicht."],
+"arbeiten" : ["Wir müssen reden.", "Ich habe aber keine Zeit. Ich muss arbeiten.", "Immer musst du arbeiten.", "Das stimmt doch gar nicht."],
+"joggen" : ["Wir müssen reden.", "Ich habe aber keine Zeit. Ich muss joggen.", "Immer musst du joggen.", "Das stimmt doch gar nicht."],
+"meditieren" : ["Wir müssen reden.", "Ich habe aber keine Zeit. Ich muss meditieren.", "Immer musst du meditieren.", "Das stimmt doch gar nicht."],
+"telefonieren" : ["Wir müssen reden.", "Ich habe aber keine Zeit. Ich muss telefonieren.", "Immer musst du telefonieren.", "Das stimmt doch gar nicht."],
+"Yoga machen" : ["Wir müssen reden.", "Ich habe aber keine Zeit. Ich muss Yoga üben.", "Immer musst du Yoga üben.", "Das stimmt doch gar nicht."],
+"schlafen" : ["Wir müssen reden.", "Ich habe aber keine Zeit. Ich muss schlafen.", "Immer musst du schlafen.", "Das stimmt doch gar nicht."],
+"demonstrieren" : ["Wir müssen reden.", "Ich habe aber keine Zeit. Ich muss demonstrieren.", "Immer musst du demonstrieren.", "Das stimmt doch gar nicht."],
+"Fußball gucken" : ["Wir müssen reden.", "Ich habe aber keine Zeit. Ich muss Fußball gucken.", "Immer musst du Fußball gucken.", "Das stimmt doch gar nicht."],
+"trainieren" : ["Wir müssen reden.", "Ich habe aber keine Zeit. Ich muss trainieren.", "Immer musst du trainieren.", "Das stimmt doch gar nicht."]}
+]`;
+
+const triggers = ['lernen', 'arbeiten', 'joggen', 'meditieren', 'telefonieren', 'Yoga machen', 'schlafen', 'demonstrieren', 'Fußball gucken', 'trainieren'];
