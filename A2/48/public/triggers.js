0df823b3d54c946c97e7d7fa6542ae6f22e81bbb
smarterGerman <65780492+smarterGerman@users.noreply.github.com>
2022-12-16 11:42:37 -0500

    Add files via upload

---
+++ A2/48/public/triggers.js
@@ -0,0 +1,14 @@
+let allTriggerAnswersJSON = `[
+{"Computer (m)" : ["Magst du meinen neuen Computer?", "Welchen Computer meinst du denn?", "Na den Computer, den ich gestern gekauft habe.", "Den Computer, den du gestern gekauft hast, mag ich nicht."],
+"Auto (n)" : ["Magst du mein neues Auto?", "Welches Auto meinst du denn?", "Na das Auto, das ich gestern gekauft habe.", "Das Auto, das du gestern gekauft hast, mag ich nicht."],
+"Handtasche (f)" : ["Magst du meine neue Handtasche?", "Welche Handtasche meinst du denn?", "Na die Handtasche, die ich gestern gekauft habe.", "Die Handtasche, die du gestern gekauft hast, mag ich nicht."],
+"Schuhe (pl)" : ["Magst du meine neuen Schuhe?", "Welche Schuhe meinst du denn", "Na die Schuhe, die ich gestern gekauft habe.", "Die Schuhe, die du gestern gekauft hast, mag ich nicht."],
+"Fairphone (n)" : ["Magst du mein neues Fairphone?", "Welches Fairphone meinst du denn?", "Na das Fairphone, das ich gestern gekauft habe.", "Das Fairphone, das du gestern gekauft hast, mag ich nicht."],
+"Wohnung (f)" : ["Magst du meine neue Wohnung?", "Welche Wohnung meinst du denn?", "Na, die Wohnung, die ich gestern gekauft habe.", "Die Wohnung, die du gestern gekauft hast, mag ich nicht."],
+"Fahrrad (n)" : ["Magst du mein neues Fahrrad?", "Welches Fahrrad meinst du denn?", "Na das Fahrrad, das ich gestern gekauft habe.", "Das Fahrrad, das du gestern gekauft hast, mag ich nicht."],
+"Kleid (n)" : ["Magst du mein Kleid?", "Welches Kleid meinst du denn?", "Na das Kleid, das ich gestern gekauft habe.", "Das Kleid, das du gestern gekauft hast, mag ich nicht."],
+"Sonnenbrille (f)" : ["Magst du meine neue Sonnenbrille?", "Welche Sonnenbrille meinst du denn?", "Na die Sonnenbrille, die ich gestern gekauft habe.", "Die Sonnenbrille, die du gestern gekauft hast, mag ich nicht."],
+"Anzug (pl)" : ["Magst du meine neuen Anzüge?", "Welche Anzüge meinst du denn?", "Na die Anzüge, die ich gestern gekauft habe.", "Die Anzüge, die du gestern gekauft hast, mag ich nicht."]}
+]`;
+
+const triggers = ['Computer (m)', 'Auto (n)', 'Handtasche (f)', 'Schuhe (pl)', 'Fairphone (n)', 'Wohnung (f)', 'Fahrrad (n)', 'Kleid (n)', 'Sonnenbrille (f)', 'Anzug (pl)'];
