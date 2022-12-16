ee009840b46380143dcef57ee1a6558110201850
smarterGerman <65780492+smarterGerman@users.noreply.github.com>
2022-12-16 11:32:10 -0500

    Add files via upload

---
+++ A1/15/public/triggers.js
@@ -0,0 +1,14 @@
+let allTriggerAnswersJSON = `[
+{"groß | Hund | klein" : ["Ich habe einen kleinen Hund.", "Ein kleiner Hund ist praktisch.", "Ist ein großer Hund auch praktisch?", "Ein großer Hund ist nicht so praktisch."],
+"klein | groß | Handy" : ["Ich habe ein kleines Handy.", "Ein kleines Handy ist praktisch.", "Ist ein großes Handy auch praktisch?", "Ein großes Handy ist nicht so praktisch."],
+"klein  | groß | Tasche" : ["Ich habe eine kleine Tasche.", "Eine kleine Tasche ist praktisch.", "Ist eine große Tasche auch praktisch?", "Eine große Tasche ist nicht so praktisch."],
+"groß | Auto | klein" : ["Ich habe ein kleines Auto.", "Ein kleines Auto ist praktisch.", "Ist ein großes Auto auch praktisch?", "Ein großes Auto ist nicht so praktisch."],
+"klein | groß | Portemonnaie" : ["Ich habe ein kleines Portemonnaie.", "Ein kleines Portemonnaie ist praktisch.", "Ist ein großes Portemonnaie auch praktisch?", "Ein großes Portemonnaie ist nicht so praktisch."],
+"klein | groß | Wohnung" : ["Ich habe eine kleine Wohnung.", "Eine kleine Wohnung ist praktisch.", "Ist eine große Wohnung auch praktisch?", "Eine große Wohnung ist nicht so praktisch."],
+"klein | groß | Schüssel" : ["Ich habe eine kleine Schüssel.", "Eine kleine Schüssel ist praktisch.", "Ist eine große Schüssel auch praktisch?", "Eine große Schüssel ist nicht so praktisch."],
+"klein | groß | Fahrrad" : ["Ich habe ein kleines Fahrrad.", "Ein kleines Fahrrad ist praktisch.", "Ist ein großes Fahrrad auch praktisch?", "Ein großes Fahrrad ist nicht so praktisch."],
+"klein | groß | Computer" : ["Ich habe einen kleinen Computer.", "Ein kleiner Computer ist praktisch.", "Ist ein großer Computer auch praktisch?", "Ein großer Computer ist nicht so praktisch."],
+"klein | groß | Kissen" : ["Ich habe ein kleines Kissen.", "Ein kleines Kissen ist praktisch.", "Ist ein großes Kissen auch praktisch?", "Ein großes Kissen ist nicht so praktisch."]}
+]`;
+
+const triggers = ['groß | Hund | klein', 'klein | groß | Handy', 'klein  | groß | Tasche', 'groß | Auto | klein', 'klein | groß | Portemonnaie', 'klein | groß | Wohnung', 'klein | groß | Schüssel', 'klein | groß | Fahrrad', 'klein | groß | Computer', 'klein | groß | Kissen'];
