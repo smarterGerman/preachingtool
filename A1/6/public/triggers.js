d78d486cff5f585856d7889466abd4e62da02e51
smarterGerman <65780492+smarterGerman@users.noreply.github.com>
2022-12-21 16:21:08 -0500

    Add files via upload

--- A1/6/public/triggers.js
+++ A1/6/public/triggers.js
@@ -6,7 +6,7 @@ let allTriggerAnswersJSON = `[
 "Salat" : ["Möchtest du einen frischen Salat?", "Ich möchte keinen frischen Salat.", "Was? Du möchtest keinen frischen Salat?", "Nein, ich möchte lieber keinen frischen Salat."],
 "Apfel" : ["Möchtest du einen frischen Apfel?", "Ich möchte keinen frischen Apfel.", "Was? Du möchtest keinen frischen Apfel?", "Nein, ich möchte lieber keinen frischen Apfel."],
 "Stück Kuchen" : ["Möchtest du ein frisches Stück Kuchen?", "Ich möchte kein frisches Stück Kuchen.", "Was? Du möchtest kein frisches Stück Kuchen?", "Nein, ich möchte lieber kein frisches Stück Kuchen."],
-"Feige" : ["Möchtest du eine frische Feige?", "Nein, ich möchte keine frische Feige.", "Was? Du möchtest keine frische Feige?", "Nein, ich möchte lieber keine frische Feige."],
+"Feige" : ["Möchtest du eine frische Feige?", "Ich möchte keine frische Feige.", "Was? Du möchtest keine frische Feige?", "Nein, ich möchte lieber keine frische Feige."],
 "Handtuch" : ["Möchtest du ein frisches Handtuch?", "Ich möchte kein frisches Handtuch.", "Was? Du möchtest kein frisches Handtuch?", "Nein, ich möchte lieber kein frisches Handtuch."],
 "Waffel" : ["Möchtest du eine frische Waffel?", "Ich möchte keine frische Waffel.", "Was? Du möchtest keine frische Waffel?", "Nein, ich möchte lieber keine frische Waffel."]}
 ]`;
