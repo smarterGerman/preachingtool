d3a766e96539a16e113c412d5946e48accb1e8e5
smarterGerman <65780492+smarterGerman@users.noreply.github.com>
2023-01-26 08:26:08 -0500

    Add files via upload

--- A2/15/triggers.js
+++ A2/15/triggers.js
@@ -5,10 +5,10 @@ let allTriggerAnswersJSON = `[
 "Hund" : ["Hättest du lieber einen großen oder kleinen Hund?", "Ich hätte lieber einen kleinen Hund.", "Warum hättest du denn lieber einen kleinen Hund?", "Weil ich einfach lieber einen kleinen Hund hätte."],
 "Haus" : ["Hättest du lieber ein großes oder kleines Haus?", "Ich hätte lieber ein kleines Haus.", "Warum hättest du denn lieber ein kleines Haus?", "Weil ich einfach lieber ein kleines Haus hätte."],
 "Wagen" : ["Hättest du lieber einen Neuwagen oder Gebrauchtwagen?", "Ich hätte lieber einen Gebrauchtwagen.", "Warum hättest du denn lieber einen Gebrauchtwagen?", "Weil ich einfach lieber einen Gebrauchtwagen hätte."],
-"Katze" : ["Hättest du lieber einen junge oder alte Katze?", "Ich hätte lieber eine alte Katze.", "Warum hättest du denn lieber eine alte Katze?", "Weil ich einfach lieber eine alt Katze hätte."],
+"Katze" : ["Hättest du lieber eine junge oder alte Katze?", "Ich hätte lieber eine alte Katze.", "Warum hättest du denn lieber eine alte Katze?", "Weil ich einfach lieber eine alte Katze hätte."],
 "Anzug" : ["Hättest du lieber einen neuen oder gebrauchten Anzug?", "Ich hätte lieber einen gebrauchten Anzug.", "Warum hättest du denn lieber einen gebrauchten Anzug?", "Weil ich einfach lieber einen gebrauchten Anzug hätte."],
 "Garten" : ["Hättest du lieber einen großen oder kleinen Garten?", "Ich hätte lieber einen kleinen Garten.", "Warum hättest du denn lieber einen kleinen Garten?", "Weil ich einfach lieber einen kleinen Garten hätte."],
-"Pizza" : ["Hättest du lieber einen heiße oder kalte Pizza?", "Ich hätte lieber eine kalte Pizza.", "Warum hättest du denn lieber eine kalte Pizza?", "Weil ich einfach lieber einen kalte Pizza hätte."]}
+"Pizza" : ["Hättest du lieber eine heiße oder kalte Pizza?", "Ich hätte lieber eine kalte Pizza.", "Warum hättest du denn lieber eine kalte Pizza?", "Weil ich einfach lieber eine kalte Pizza hätte."]}
 ]`;
 
 const triggers = ['Kaffee', 'Telefon', 'Wohnung', 'Hund', 'Haus', 'Wagen', 'Katze', 'Anzug', 'Garten', 'Pizza'];
