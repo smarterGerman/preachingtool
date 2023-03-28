9bb5ac66f52175d07b219a16def5e8503aa55f50
smarterGerman <65780492+smarterGerman@users.noreply.github.com>
2023-03-28 18:48:45 +0200

    Add files via upload

--- A2/14/triggers.js
+++ A2/14/triggers.js
@@ -5,10 +5,10 @@ let allTriggerAnswersJSON = `[
 "auf + Markt | in + Stadt" : ["Wo bist du gerade?", "Ich bin auf dem Markt. Warum?", "Könntest du kurz in die Stadt kommen? Ich brauche deine Hilfe.", "Kein Problem. Ich komme in die Stadt."],
 "in + Stadt | an + Strand" : ["Wo bist du gerade?", "Ich bin in der Stadt. Warum?", "Könntest du kurz an den Strand kommen? Ich brauche deine Hilfe.", "Kein Problem. Ich komme an den Strand."],
 "an + Strand | vor + Tür" : ["Wo bist du gerade?", "Ich bin am Strand. Warum?", "Könntest du kurz vor die Tür kommen? Ich brauche deine Hilfe.", "Kein Problem. Ich komme vor die Tür."],
-"vor + Tür | hinter + Haus" : ["Wo bist du gerade?", "Ich bin vor der Tür. Warum?", "Könntest du kurz ins Haus kommen? Ich brauche deine Hilfe.", "Kein Problem. Ich komme ins Haus."],
-"hinter + Haus | unter + Dusche" : ["Wo bist du gerade?", "Ich bin im Haus. Warum?", "Könntest du kurz in die Dusche kommen? Ich brauche deine Hilfe.", "Kein Problem. Ich komme in die Dusche."],
-"unter + Dusche | auf + Klo" : ["Wo bist du gerade?", "Ich bin in der Dusche. Warum?", "Könntest du kurz aufs Klo kommen? Ich brauche deine Hilfe.", "Kein Problem. Ich komme aufs Klo."],
-"auf + Klo | auf + Dachboden" : ["Wo bist du gerade?", "Ich bin aufm Klo. Warum?", "Könntest du kurz auf den Dachboden kommen? Ich brauche deine Hilfe.", "Kein Problem. Ich komme auf den Dachboden."]}
+"vor + Tür | hinter + Haus" : ["Wo bist du gerade?", "Ich bin vor der Tür. Warum?", "Könntest du kurz hinter das Haus kommen? Ich brauche deine Hilfe.", "Kein Problem. Ich komme hinter das Haus."],
+"hinter + Haus | unter + Dusche" : ["Wo bist du gerade?", "Ich bin hinter dem Haus. Warum?", "Könntest du kurz unter die Dusche kommen? Ich brauche deine Hilfe.", "Kein Problem. Ich komme unter die Dusche."],
+"unter + Dusche | auf + Klo" : ["Wo bist du gerade?", "Ich bin unter der Dusche. Warum?", "Könntest du kurz aufs Klo kommen? Ich brauche deine Hilfe.", "Kein Problem. Ich komme aufs Klo."],
+"auf + Klo | auf + Dachboden" : ["Wo bist du gerade?", "Ich bin auf dem Klo. Warum?", "Könntest du kurz auf den Dachboden kommen? Ich brauche deine Hilfe.", "Kein Problem. Ich komme auf den Dachboden."]}
 ]`;
 
 const triggers = ['auf + Dachboden | in + Wohnzimmer', 'in + Wohnzimmer | in + Küche', 'in + Küche | auf + Markt', 'auf + Markt | in + Stadt', 'in + Stadt | an + Strand', 'an + Strand | vor + Tür', 'vor + Tür | hinter + Haus', 'hinter + Haus | unter + Dusche', 'unter + Dusche | auf + Klo', 'auf + Klo | auf + Dachboden'];
