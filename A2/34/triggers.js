d00b4bbb1fc4014091fc6300cd794d4549a3285c
smarterGerman <65780492+smarterGerman@users.noreply.github.com>
2023-03-29 12:14:25 +0200

    Add files via upload

--- A2/34/triggers.js
+++ A2/34/triggers.js
@@ -5,7 +5,7 @@ let allTriggerAnswersJSON = `[
 "sich erinnern an + Akk. | sie" : ["Erinnerst du dich an sie?", "Ich erinnere mich nicht an sie.", "Warum erinnerst du dich denn nicht an sie?", "Weil ich mich nicht an sie erinnern möchte."],
 "sich ärgern über + Akk. | er" : ["Ärgerst du dich über ihn?", "Ich ärgere mich nicht über ihn.", "Warum ärgerst du dich denn nicht über ihn?", "Weil ich mich nicht über ihn ärgern möchte."],
 "sich entscheiden gegen + Akk. | sie" : ["Entscheidest du dich gegen sie?", "Ich entscheide mich nicht gegen sie.", "Warum entscheidest du dich denn nicht gegen sie?", "Weil ich mich nicht gegen sie entscheiden möchte."],
-"sich freuen über + Akk. | er" : ["Freust du dich auf ihn?", "Ich freue mich nicht über ihn.", "Warum freust du dich denn nicht über ihn?", "Weil ich mich nicht über ihn freuen möchte."],
+"sich freuen über + Akk. | er" : ["Freust du dich über ihn?", "Ich freue mich nicht über ihn.", "Warum freust du dich denn nicht über ihn?", "Weil ich mich nicht über ihn freuen möchte."],
 "sich verlieben in + Akk. | sie" : ["Verliebst du dich in sie?", "Ich verliebe mich nicht in sie.", "Warum verliebst du dich denn nicht in sie?", "Weil ich mich nicht in sie verlieben möchte."],
 "sich verlassen auf + Akk. | sie" : ["Verlässt du dich auf sie?", "Ich verlasse mich nicht auf sie.", "Warum verlässt du dich denn nicht auf sie?", "Weil ich mich nicht auf sie verlassen möchte."],
 "sich aufregen über + Akk. | er" : ["Regst du dich über ihn auf?", "Ich rege mich nicht über ihn auf.", "Warum regst du dich denn nicht über ihn auf?", "Weil ich mich nicht über ihn aufregen möchte."]}
