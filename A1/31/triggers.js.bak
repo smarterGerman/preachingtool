b2941a43d1b3fe92d59c08a2aa8ff65954034432
smarterGerman <65780492+smarterGerman@users.noreply.github.com>
2023-04-04 15:02:40 +0200

    Add files via upload

---
+++ A1/31/triggers.js.bak
@@ -0,0 +1,14 @@
+let allTriggerAnswersJSON = `[
+{"tanzen" : ["Willst du mit mir tanzen?", "Ich will nicht mit dir tanzen.", "Aber alle wollen mit mir tanzen.", "Ich will aber nicht."],
+"Klavier spielen" : ["Willst du mit mir Klavier spielen?", "Ich will nicht mit dir Klavier spielen.", "Aber alle wollen mit mir Klavier spielen.", "Ich will aber nicht."],
+"kochen" : ["Willst du mit mir kochen?", "Ich will nicht mit dir kochen.", "Aber alle wollen mit mir kochen.", "Ich will aber nicht."],
+"zeichnen" : ["Willst du mit mir zeichnen?", "Ich will nicht mit dir zeichnen.", "Aber alle wollen mit mir zeichnen.", "Ich will aber nicht."],
+"improvisieren" : ["Willst du mit mir improvisieren?", "Ich will nicht mit dir improvisieren.", "Aber alle wollen mit mir improvisieren.", "Ich will aber nicht."],
+"Wein trinken" : ["Willst du mit mir Wein trinken?", "Ich will nicht mit dir Wein trinken.", "Aber alle wollen mit mir Wein trinken.", "Ich will aber nicht."],
+"klettern" : ["Willst du mit mir klettern?", "Ich will nicht mit dir klettern.", "Aber alle wollen mit mir klettern.", "Ich will aber nicht."],
+"Schach spielen" : ["Willst du mit mir Schach spielen?", "Ich will nicht mit dir Schach spielen.", "Aber alle wollen mit mir Schach spielen.", "Ich will aber nicht."],
+"singen" : ["Willst du mit mir singen?", "Ich will nicht mit dir singen.", "Aber alle wollen mit mir singen.", "Ich will aber nicht."],
+"angeln" : ["Willst du mit mir angeln?", "Ich will nicht mit dir angeln.", "Aber alle wollen mit mir angeln.", "Ich will aber nicht."]}
+]`;
+
+const triggers = ['tanzen', 'Klavier spielen', 'kochen', 'zeichnen', 'improvisieren', 'Wein trinken', 'klettern', 'Schach spielen', 'singen', 'angeln'];
---
+++ A1/44/triggers.js
@@ -0,0 +1,14 @@
+let allTriggerAnswersJSON = `[
+{"aussteigen | einsteigen" : ["Steigst du ein?", "Nein, ich steige nicht ein.", "Dann steigst du also aus?", "Genau! Ich steige gerade aus."],
+"zumachen | aufmachen" : ["Machst du zu?", "Nein, ich mache nicht zu.", "Dann machst du also auf?", "Genau! Ich mache gerade auf."],
+"abschließen | aufschließen" : ["Schließt du ab?", "Nein, ich schließe nicht ab.", "Dann schließt du also auf?", "Genau! Ich schließe gerade auf."],
+"ausziehen | einziehen" : ["Ziehst du aus?", "Nein, ich ziehe nicht aus.", "Dann ziehst du also ein?", "Genau! Ich ziehe gerade ein."],
+"ausladen | einladen" : ["Lädst du aus?", "Nein, ich lade nicht aus.", "Dann lädst du also ein?", "Genau! Ich lade gerade ein."],
+"rausgehen | reingehen" : ["Gehst du raus?", "Nein, ich gehe nicht raus.", "Dann gehst du also rein?", "Genau! Ich gehe gerade rein."],
+"zunehmen | abnehmen" : ["Nimmst du zu?", "Nein, ich nehme nicht zu.", "Dann nimmst du also ab?", "Genau! Ich nehme gerade ab."],
+"zurückfahren | hinfahren" : ["Fährst du zurück?", "Nein, ich fahre nicht zurück.", "Dann fährst du also hin?", "Genau! Ich fahre gerade hin."],
+"ausatmen | einatmen" : ["Atmest du aus?", "Nein, ich atme nicht aus.", "Dann atmest du also ein?", "Genau! Ich atme gerade ein."],
+"einbrechen | ausbrechen" : ["Brichst du ein?", "Nein, ich breche nicht ein.", "Dann brichst du also aus?", "Genau! Ich breche gerade aus."]}
+]`;
+
+const triggers = ['aussteigen | einsteigen', 'zumachen | aufmachen', 'abschließen | aufschließen', 'ausziehen | einziehen', 'ausladen | einladen', 'rausgehen | reingehen', 'zunehmen | abnehmen', 'zurückfahren | hinfahren', 'ausatmen | einatmen', 'einbrechen | ausbrechen'];
