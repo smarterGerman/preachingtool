183bea3962fabe1ba540c10c514d30e14ec7d3e6
smarterGerman <65780492+smarterGerman@users.noreply.github.com>
2023-04-27 17:51:00 +0200

    Add files via upload

--- A2/24/triggers.js
+++ A2/24/triggers.js
@@ -1,11 +1,11 @@
 let allTriggerAnswersJSON = `[
 {"zur Bank | Geld abheben" : ["Ich gehe zur Bank.", "Wozu gehst du zur Bank?", "Um Geld abzuheben.", "Ach so, damit du Geld abheben kannst."],
-"ins Hallenbad | schwimmen" : ["Ich gehe ins Hallenbad.", "Wozu gehst ins Hallenbad?", "Um zu schwimmen.", "Ach so, damit du schwimmen kannst."],
+"ins Hallenbad | schwimmen" : ["Ich gehe ins Hallenbad.", "Wozu gehst du ins Hallenbad?", "Um zu schwimmen.", "Ach so, damit du schwimmen kannst."],
 "zu Edeka | Kartoffeln kaufen" : ["Ich gehe zu Edeka.", "Wozu gehst du zu Edeka?", "Um Kartoffeln zu kaufen.", "Ach so, damit du Kartoffeln kaufen kannst."],
 "zur Schule | Deutsch lernen" : ["Ich gehe zur Schule.", "Wozu gehst du zur Schule?", "Um Deutsch zu lernen.", "Ach so, damit du Deutsch lernen kannst."],
 "ins Gebetshaus | beten" : ["Ich gehe ins Gebetshaus.", "Wozu gehst du ins Gebetshaus?", "Um zu beten.", "Ach so, damit du beten kannst."],
 "ins Büro | arbeiten" : ["Ich gehe ins Büro.", "Wozu gehst du ins Büro?", "Um zu arbeiten.", "Ach so, damit du arbeiten kannst."],
-"zur Demo | demonstrieren" : ["Ich gehe auf die Demo.", "Wozu gehst du auf die Demo?", "Um zu demonstrieren.", "Ach so, damit du demonstrieren kannst."],
+"zur Demo | demonstrieren" : ["Ich gehe zur Demo.", "Wozu gehst du zur Demo?", "Um zu demonstrieren.", "Ach so, damit du demonstrieren kannst."],
 "ins Kino | Film anschauen" : ["Ich gehe ins Kino.", "Wozu gehst du ins Kino?", "Um einen Film anzuschauen.", "Ach so, damit du einen Film anschauen kannst."],
 "auf den Markt | Gemüse kaufen" : ["Ich gehe auf den Markt.", "Wozu gehst du auf den Markt?", "Um Gemüse zu kaufen.", "Ach so, damit du Gemüse kaufen kannst."],
 "ins Bett | schlafen" : ["Ich gehe ins Bett.", "Wozu gehst du ins Bett?", "Um zu schlafen.", "Ach so, damit du schlafen kannst."]}
