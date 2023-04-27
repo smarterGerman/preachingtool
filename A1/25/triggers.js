7cd734b296da68336e6d81f6cacb67f2e9863ff9
smarterGerman <65780492+smarterGerman@users.noreply.github.com>
2023-04-27 17:48:59 +0200

    Add files via upload

--- A1/25/triggers.js
+++ A1/25/triggers.js
@@ -2,8 +2,8 @@ let allTriggerAnswersJSON = `[
 {"kennen" : ["Glaubst du, der kennt uns?", "Der kennt uns nicht.", "Na dann ist ja gut. Ich dachte schon, der kennt uns.", "Aber ich kenne euch doch."],
 "rufen" : ["Glaubst du, der ruft uns?", "Der ruft uns nicht.", "Na dann ist ja gut. Ich dachte schon, der ruft uns.", "Aber ich rufe euch doch."],
 "beobachten" : ["Glaubst du, der beobachtet uns?", "Der beobachtet uns nicht.", "Na dann ist ja gut. Ich dachte schon, der beobachtet uns.", "Aber ich beobachte euch doch."],
-"siezen" : ["Glaubst du, der siezt uns?", "Der siezt uns nicht.", "Na dann ist ja gut. Ich dachte schon, der siezt uns.", "Aber ich sieze euch doch."],
-"duzen" : ["Glaubst du, der duzt uns?", "Der duzt uns nicht.", "Na dann ist ja gut. Ich dachte schon, der duzt uns.", "Aber ich duze euch doch."],
+"hassen" : ["Glaubst du, der hasst uns?", "Der hasst uns nicht.", "Na dann ist ja gut. Ich dachte schon, der hasst uns.", "Aber ich hasse euch doch."],
+"vergessen" : ["Glaubst du, der vergisst uns?", "Der vergisst uns nicht.", "Na dann ist ja gut. Ich dachte schon, der vergisst uns.", "Aber ich vergesse euch doch."],
 "winken" : ["Glaubst du, der winkt uns?", "Der winkt uns nicht.", "Na dann ist ja gut. Ich dachte schon, der winkt uns.", "Aber ich winke euch doch."],
 "ver.arsch.en" : ["Glaubst du, der verarscht uns?", "Der verarscht uns nicht.", "Na dann ist ja gut. Ich dachte schon, der verarscht uns.", "Aber ich verarsche euch doch."],
 "besuchen" : ["Glaubst du, der besucht uns?", "Der besucht uns nicht.", "Na dann ist ja gut. Ich dachte schon, der besucht uns.", "Aber ich besuche euch doch."],
@@ -11,4 +11,4 @@ let allTriggerAnswersJSON = `[
 "hören" : ["Glaubst du, der hört uns?", "Der hört uns nicht.", "Na dann ist ja gut. Ich dachte schon, der hört uns.", "Aber ich höre euch doch."]}
 ]`;
 
-const triggers = ['kennen', 'rufen', 'beobachten', 'siezen', 'duzen', 'winken', 'ver.arsch.en', 'besuchen', 'meinen', 'hören'];
+const triggers = ['kennen', 'rufen', 'beobachten', 'hassen', 'vergessen', 'winken', 'ver.arsch.en', 'besuchen', 'meinen', 'hören'];
