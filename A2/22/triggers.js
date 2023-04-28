a65096848da96559b57e5097e080eac73fc3a206
smarterGerman <65780492+smarterGerman@users.noreply.github.com>
2023-04-28 13:46:03 +0200

    Add files via upload

--- A2/22/triggers.js
+++ A2/22/triggers.js
@@ -1,14 +1,14 @@
 let allTriggerAnswersJSON = `[
-{"müde sein | einen Kaffee trinken" : ["Was machst du, wenn du müde bist?", "Dann trinke ich einen Kaffee.", "Und du? Was machst du, wenn du müde bist?", "Wenn ich müde bin, dann trinke ich meistens auch einen Kaffee."],
-"gestresst sein | meditieren" : ["Was machst du, wenn du gestresst bist?", "Dann meditiere ich.", "Und du? Was machst du, wenn du gestresst bist?", "Wenn ich gestresst bin, dann meditiere ich meistens auch."],
-"erkältet sein | Tee trinken" : ["Was machst du, wenn du erkältet bist?", "Dann trinke ich Tee.", "Und du? Was machst du, wenn du erkältet bist?", "Wenn ich erkältet bin, dann trinke ich meistens auch Tee."],
-"traurig sein | Klavier spielen" : ["Was machst du, wenn du traurig bist?", "Dann spiele ich Klavier.", "Und du? Was machst du, wenn du traurig bist?", "Wenn ich traurig bin, dann spiele ich meistens auch Klavier."],
-"verliebt sein | verrückte Sachen machen" : ["Was machst du, wenn du verliebt bist?", "Dann mache ich verrückte Sachen.", "Und du? Was machst du, wenn du verliebt bist?", "Wenn ich verliebt bin, dann mache ich meistens auch verrückte Sachen."],
-"wütend sein | laut werden" : ["Was machst du, wenn du wütend bist?", "Dann werde ich laut.", "Und du? Was machst du, wenn du wütend bist?", "Wenn ich wütend bin, dann werde ich meistens auch laut."],
-"nervös sein | meine Katze streicheln" : ["Was machst du, wenn du nervös bist?", "Dann streichel ich meine Katze.", "Und du? Was machst du, wenn du nervös bist?", "Wenn ich nervös bin, dann streichel ich auch meistens meine Katze."],
-"lustlos sein | sich eine Massage gönnen" : ["Was machst du, wenn du lustlos bist?", "Dann gönne ich mir eine Massage.", "Und du? Was machst du, wenn du lustlos bist?", "Wenn ich lustlos bin, dann gönne ich mir meistens auch eine Massage."],
-"glücklich sein | ein Lied pfeifen" : ["Was machst du, wenn du glücklich bist?", "Dann pfeife ich ein Lied.", "Und du? Was machst du, wenn du glücklich bist?", "Wenn ich glücklich bin, dann pfeife ich meistens auch ein Lied."],
-"krank sein | sich ins Bett legen" : ["Was machst du, wenn du krank bist?", "Dann lege ich mich ins Bett.", "Und du? Was machst du, wenn du krank bist?", "Wenn ich krank bin, dann lege ich mich meistens auch ins Bett."]}
+{"müde sein | einen Kaffee trinken" : ["Was machst du, wenn du müde bist?", "Dann trinke ich einen Kaffee.", "Und du? Was machst du, wenn du müde bist?", "Wenn ich müde bin, trinke ich meistens auch einen Kaffee."],
+"gestresst sein | meditieren" : ["Was machst du, wenn du gestresst bist?", "Dann meditiere ich.", "Und du? Was machst du, wenn du gestresst bist?", "Wenn ich gestresst bin, meditiere ich meistens auch."],
+"erkältet sein | Tee trinken" : ["Was machst du, wenn du erkältet bist?", "Dann trinke ich Tee.", "Und du? Was machst du, wenn du erkältet bist?", "Wenn ich erkältet bin, trinke ich meistens auch Tee."],
+"traurig sein | Klavier spielen" : ["Was machst du, wenn du traurig bist?", "Dann spiele ich Klavier.", "Und du? Was machst du, wenn du traurig bist?", "Wenn ich traurig bin, spiele ich meistens auch Klavier."],
+"verliebt sein | verrückte Sachen machen" : ["Was machst du, wenn du verliebt bist?", "Dann mache ich verrückte Sachen.", "Und du? Was machst du, wenn du verliebt bist?", "Wenn ich verliebt bin, mache ich meistens auch verrückte Sachen."],
+"wütend sein | laut werden" : ["Was machst du, wenn du wütend bist?", "Dann werde ich laut.", "Und du? Was machst du, wenn du wütend bist?", "Wenn ich wütend bin, werde ich meistens auch laut."],
+"nervös sein | meine Katze streicheln" : ["Was machst du, wenn du nervös bist?", "Dann streichel ich meine Katze.", "Und du? Was machst du, wenn du nervös bist?", "Wenn ich nervös bin, streichel ich auch meistens meine Katze."],
+"lustlos sein | sich eine Massage gönnen" : ["Was machst du, wenn du lustlos bist?", "Dann gönne ich mir eine Massage.", "Und du? Was machst du, wenn du lustlos bist?", "Wenn ich lustlos bin, gönne ich mir meistens auch eine Massage."],
+"glücklich sein | ein Lied pfeifen" : ["Was machst du, wenn du glücklich bist?", "Dann pfeife ich ein Lied.", "Und du? Was machst du, wenn du glücklich bist?", "Wenn ich glücklich bin, pfeife ich meistens auch ein Lied."],
+"krank sein | sich ins Bett legen" : ["Was machst du, wenn du krank bist?", "Dann lege ich mich ins Bett.", "Und du? Was machst du, wenn du krank bist?", "Wenn ich krank bin, lege ich mich meistens auch ins Bett."]}
 ]`;
 
 const triggers = ['erkältet sein | Tee trinken', 'gestresst sein | meditieren', 'erkältet sein Ingwertee mit Zitrone trinken', 'traurig sein | Klavier spielen', 'verliebt sein | verrückte Sachen machen', 'wütend sein | laut werden', 'nervös sein | meine Katze streicheln', 'lustlos sein | sich eine Massage gönnen', 'glücklich sein | ein Lied pfeifen', 'krank sein | sich ins Bett legen'];
