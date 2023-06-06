772a82c981365adee33002bc76ff32f189e4cd01
smarterGerman <65780492+smarterGerman@users.noreply.github.com>
2023-06-06 12:46:00 +0200

    Add files via upload

--- A2/38/triggers.js
+++ A2/38/triggers.js
@@ -1,8 +1,8 @@
 let allTriggerAnswersJSON = `[
 {"Tesla" : ["Wem gehört denn der Tesla dort?", "Das ist unser Tesla.", "Ach, ich dachte, das sei euer Tesla.", "Hab ich doch gesagt. Unser Tesla."],
-"Kind" : ["Wem gehört denn das Kind dort?", "Das ist unser Kind.", "Ach, ich dachte, das sei euer Kind.", "Hab ich doch gesagt. Unser Kind."],
+"zu wem + Kind" : ["Zu wem gehört denn das Kind dort?", "Das ist unser Kind.", "Ach, ich dachte, das sei euer Kind.", "Hab ich doch gesagt. Unser Kind."],
 "Katze" : ["Wem gehört denn die Katze dort?", "Das ist unsere Katze.", "Ach, ich dachte, das sei eure Katze.", "Hab ich doch gesagt. Unsere Katze."],
-"Kinder (pl.)" : ["Wem gehören denn die Kinder dort?", "Das sind unsere Kinder.", "Ach, ich dachte, das seien eure Kinder.", "Hab ich doch gesagt. Unsere Kinder."],
+"zu wem + Kinder (pl.)" : ["Zu wem gehören denn die Kinder dort?", "Das sind unsere Kinder.", "Ach, ich dachte, das seien eure Kinder.", "Hab ich doch gesagt. Unsere Kinder."],
 "Fahrräder (pl.)" : ["Wem gehören denn die Fahrräder dort?", "Das sind unsere Fahrräder.", "Ach, ich dachte, das seien eure Fahrräder.", "Hab ich doch gesagt. Unsere Fahrräder."],
 "Fahrradtasche" : ["Wem gehört denn die Fahrradtasche dort?", "Das ist unsere Fahrradtasche.", "Ach, ich dachte, das sei eure Fahrradtasche.", "Hab ich doch gesagt. Unsere Fahrradtasche."],
 "Müll" : ["Wem gehört denn der Müll dort?", "Das ist unser Müll.", "Ach, ich dachte, das sei euer Müll.", "Hab ich doch gesagt. Unser Müll."],
@@ -11,4 +11,4 @@ let allTriggerAnswersJSON = `[
 "Koffer" : ["Wem gehört denn der Koffer dort?", "Das ist unser Koffer.", "Ach, ich dachte, das sei euer Koffer.", "Hab ich doch gesagt. Unser Koffer."]}
 ]`;
 
-const triggers = ['Tesla', 'Kind', 'Katze', 'Kinder (pl.)', 'Fahrräder (pl.)', 'Fahrradtasche', 'Müll', 'Grill', 'Schuhe (pl)', 'Koffer'];
+const triggers = ['Tesla', 'zu wem + Kind', 'Katze', 'zu wem + Kinder (pl.)', 'Fahrräder (pl.)', 'Fahrradtasche', 'Müll', 'Grill', 'Schuhe (pl)', 'Koffer'];
