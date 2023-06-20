33f0496a6779ef2f1c4e2c60e994cd312d8b958f
smarterGerman <65780492+smarterGerman@users.noreply.github.com>
2023-06-20 12:31:50 +0200

    Add files via upload

--- A2/38/triggers.js
+++ A2/38/triggers.js
@@ -1,14 +1,14 @@
 let allTriggerAnswersJSON = `[
-{"Tesla" : ["Wem gehört denn der Tesla dort?", "Das ist unser Tesla.", "Ach, ich dachte, das sei euer Tesla.", "Hab ich doch gesagt. Unser Tesla."],
-"zu wem + Kind" : ["Zu wem gehört denn das Kind dort?", "Das ist unser Kind.", "Ach, ich dachte, das sei euer Kind.", "Hab ich doch gesagt. Unser Kind."],
-"Katze" : ["Wem gehört denn die Katze dort?", "Das ist unsere Katze.", "Ach, ich dachte, das sei eure Katze.", "Hab ich doch gesagt. Unsere Katze."],
-"zu wem + Kinder (pl.)" : ["Zu wem gehören denn die Kinder dort?", "Das sind unsere Kinder.", "Ach, ich dachte, das seien eure Kinder.", "Hab ich doch gesagt. Unsere Kinder."],
-"Fahrräder (pl.)" : ["Wem gehören denn die Fahrräder dort?", "Das sind unsere Fahrräder.", "Ach, ich dachte, das seien eure Fahrräder.", "Hab ich doch gesagt. Unsere Fahrräder."],
-"Fahrradtasche" : ["Wem gehört denn die Fahrradtasche dort?", "Das ist unsere Fahrradtasche.", "Ach, ich dachte, das sei eure Fahrradtasche.", "Hab ich doch gesagt. Unsere Fahrradtasche."],
-"Müll" : ["Wem gehört denn der Müll dort?", "Das ist unser Müll.", "Ach, ich dachte, das sei euer Müll.", "Hab ich doch gesagt. Unser Müll."],
-"Grill" : ["Wem gehört denn der Grill dort?", "Das ist unser Grill.", "Ach, ich dachte, das sei euer Grill.", "Hab ich doch gesagt. Unser Grill."],
-"Schuhe (pl)" : ["Wem gehören denn die Schuhe dort?", "Das sind unsere Schuhe.", "Ach, ich dachte, das seien eure Schuhe.", "Hab ich doch gesagt. Unsere Schuhe."],
-"Koffer" : ["Wem gehört denn der Koffer dort?", "Das ist unser Koffer.", "Ach, ich dachte, das sei euer Koffer.", "Hab ich doch gesagt. Unser Koffer."]}
+{"Tesla" : ["Wem gehört denn der Tesla dort?", "Das ist unser Tesla.", "Ach, ich dachte, das sei euer Tesla.", "Habe ich doch gesagt. Unser Tesla."],
+"zu wem + Kind" : ["Zu wem gehört denn das Kind dort?", "Das ist unser Kind.", "Ach, ich dachte, das sei euer Kind.", "Habe ich doch gesagt. Unser Kind."],
+"Katze" : ["Wem gehört denn die Katze dort?", "Das ist unsere Katze.", "Ach, ich dachte, das sei eure Katze.", "Habe ich doch gesagt. Unsere Katze."],
+"zu wem + Kinder (pl.)" : ["Zu wem gehören denn die Kinder dort?", "Das sind unsere Kinder.", "Ach, ich dachte, das seien eure Kinder.", "Habe ich doch gesagt. Unsere Kinder."],
+"Fahrräder (pl.)" : ["Wem gehören denn die Fahrräder dort?", "Das sind unsere Fahrräder.", "Ach, ich dachte, das seien eure Fahrräder.", "Habe ich doch gesagt. Unsere Fahrräder."],
+"Fahrradtasche" : ["Wem gehört denn die Fahrradtasche dort?", "Das ist unsere Fahrradtasche.", "Ach, ich dachte, das sei eure Fahrradtasche.", "Habe ich doch gesagt. Unsere Fahrradtasche."],
+"Müll" : ["Wem gehört denn der Müll dort?", "Das ist unser Müll.", "Ach, ich dachte, das sei euer Müll.", "Habe ich doch gesagt. Unser Müll."],
+"Grill" : ["Wem gehört denn der Grill dort?", "Das ist unser Grill.", "Ach, ich dachte, das sei euer Grill.", "Habe ich doch gesagt. Unser Grill."],
+"Schuhe (pl)" : ["Wem gehören denn die Schuhe dort?", "Das sind unsere Schuhe.", "Ach, ich dachte, das seien eure Schuhe.", "Habe ich doch gesagt. Unsere Schuhe."],
+"Koffer" : ["Wem gehört denn der Koffer dort?", "Das ist unser Koffer.", "Ach, ich dachte, das sei euer Koffer.", "Habe ich doch gesagt. Unser Koffer."]}
 ]`;
 
 const triggers = ['Tesla', 'zu wem + Kind', 'Katze', 'zu wem + Kinder (pl.)', 'Fahrräder (pl.)', 'Fahrradtasche', 'Müll', 'Grill', 'Schuhe (pl)', 'Koffer'];
