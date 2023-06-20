31ef708a79879f4c85578b7f74f8d73ca165a297
smarterGerman <65780492+smarterGerman@users.noreply.github.com>
2023-06-20 11:43:25 +0200

    Add files via upload

--- B1/21/triggers.js
+++ B1/21/triggers.js
@@ -1,14 +1,14 @@
 let allTriggerAnswersJSON = `[
-{"Mann | jung" : ["Ich habe einen alten Mann.", "Er ist älter als ich.", "Also habe ich einen älteren Mann.", "Und das gefällt mir."],
+{"Mann | jung" : ["Ich habe einen Mann.", "Er ist älter als ich.", "Also habe ich einen älteren Mann.", "Und das gefällt mir."],
 "Kind | intelligent" : ["Ich habe ein Kind.", "Es ist intelligenter als ich.", "Also habe ich ein intelligenteres Kind.", "Und das gefällt mir."],
 "Frau | alt" : ["Ich habe eine Frau.", "Sie ist älter als ich.", "Also habe ich eine ältere Frau.", "Und das gefällt mir."],
-"Nachbarin | neugierig" : ["Ich habe eine neugierige Nachbarin.", "Sie ist neugieriger als ich.", "Also habe ich eine neugierigere Nachbarin.", "Und das gefällt mir."],
-"Chefin | geizig" : ["Ich habe eine geizige Chefin.", "Sie ist geiziger als ich.", "Also habe ich eine geizigere Chefin.", "Und das gefällt mir."],
-"Kollege| fleißig" : ["Ich habe einen fleißigen Kollegen.", "Er ist fleißiger als ich.", "Also habe ich einen fleißigeren Kollegen.", "Und das gefällt mir."],
-"Arzt | freundlich" : ["Ich habe einen freundlichen Arzt.", "Er ist freundlicher als ich.", "Also habe ich einen freundlicheren Arzt.", "Und das gefällt mir."],
-"Mutter | großzügig" : ["Ich habe eine großzügige Mutter.", "Sie ist großzügiger als ich.", "Also habe ich eine großzügigere Mutter.", "Und das gefällt mir."],
-"Tante| nervig" : ["Ich habe eine nervige Tante.", "Sie ist nerviger als ich.", "Also habe ich eine nervigere Tante.", "Und das gefällt mir."],
-"Tochter | erwachsen" : ["Ich habe eine erwachsene Tochter.", "Sie ist erwachsener als ich.", "Also habe ich eine erwachsenere Tochter.", "Und das gefällt mir."]}
+"Nachbarin | neugierig" : ["Ich habe eine Nachbarin.", "Sie ist neugieriger als ich.", "Also habe ich eine neugierigere Nachbarin.", "Und das gefällt mir."],
+"Chefin | geizig" : ["Ich habe eine Chefin.", "Sie ist geiziger als ich.", "Also habe ich eine geizigere Chefin.", "Und das gefällt mir."],
+"Kollege| fleißig" : ["Ich habe einen Kollegen.", "Er ist fleißiger als ich.", "Also habe ich einen fleißigeren Kollegen.", "Und das gefällt mir."],
+"Arzt | freundlich" : ["Ich habe einen Arzt.", "Er ist freundlicher als ich.", "Also habe ich einen freundlicheren Arzt.", "Und das gefällt mir."],
+"Mutter | großzügig" : ["Ich habe eine Mutter.", "Sie ist großzügiger als ich.", "Also habe ich eine großzügigere Mutter.", "Und das gefällt mir."],
+"Tante| nervig" : ["Ich habe eine Tante.", "Sie ist nerviger als ich.", "Also habe ich eine nervigere Tante.", "Und das gefällt mir."],
+"Tochter | erwachsen" : ["Ich habe eine Tochter.", "Sie ist erwachsener als ich.", "Also habe ich eine erwachsenere Tochter.", "Und das gefällt mir."]}
 ]`;
 
 const triggers = ['Mann | jung', 'Kind | intelligent', 'Frau | alt', 'Nachbarin | neugierig', 'Chefin | geizig', 'Kollege| fleißig', 'Arzt | freundlich', 'Mutter | großzügig', 'Tante| nervig', 'Tochter | erwachsen'];
