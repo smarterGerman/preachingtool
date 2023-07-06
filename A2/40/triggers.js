6c9dfcd8392eea17226011b74c501457097883ef
smarterGerman <65780492+smarterGerman@users.noreply.github.com>
2023-07-06 08:35:02 +0200

    Pushi it, ahhhh

--- A2/40/triggers.js
+++ A2/40/triggers.js
@@ -2,7 +2,7 @@ let allTriggerAnswersJSON = `[
 {"Kino | renovieren" : ["Hast du schon gehört? Das Metropolis wird geschlossen.", "Warum wird das Metropolis denn geschlossen?", "Es wird endlich renoviert.", "Das wurde aber auch Zeit!"],
 "Handtaschenmuseum | restaurieren" : ["Hast du schon gehört? Das Handtaschenmuseum wird geschlossen.", "Warum wird das Handtaschenmuseum denn geschlossen?", "Es wird endlich restauriert.", "Das wurde aber auch Zeit!"],
 "Kindergarten | umbauen" : ["Hast du schon gehört? Der Kindergarten wird geschlossen.", "Warum wird der Kindergarten denn geschlossen?", "Er wird endlich umgebaut.", "Das wurde aber auch Zeit!"],
-"Stadtpark | vergrößern" : ["Hast du schon gehört? Der Stadtpark wird geschlossen.", "Warum wird das Metropolis denn geschlossen?", "Er wird endlich vergrößert.", "Das wurde aber auch Zeit!"],
+"Stadtpark | vergrößern" : ["Hast du schon gehört? Der Stadtpark wird geschlossen.", "Warum wird der Stadtpark denn geschlossen?", "Er wird endlich vergrößert.", "Das wurde aber auch Zeit!"],
 "Wurstfabrik | abreißen" : ["Hast du schon gehört? Die Wurstfabrik wird geschlossen.", "Warum wird die Wurstfabrik denn geschlossen?", "Sie wird endlich abgerissen.", "Das wurde aber auch Zeit!"],
 "Flughafen | ausbauen" : ["Hast du schon gehört? Der Flughafen wird geschlossen.", "Warum wird der Flughafen denn geschlossen?", "Er wird endlich ausgebaut.", "Das wurde aber auch Zeit!"],
 "Musikschule | streichen" : ["Hast du schon gehört? Die Musikschule wird geschlossen.", "Warum wird die Musikschule denn geschlossen?", "Sie wird endlich gestrichen.", "Das wurde aber auch Zeit!"],
