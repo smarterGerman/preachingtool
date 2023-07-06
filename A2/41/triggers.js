be2b39cea2cd199e1cd0600b7b7f808267a5dc99
smarterGerman <65780492+smarterGerman@users.noreply.github.com>
2023-07-06 12:00:30 +0200

    6. Juli

--- A2/41/triggers.js
+++ A2/41/triggers.js
@@ -5,7 +5,7 @@ let allTriggerAnswersJSON = `[
 "Weißwurst | Kalbfleisch" : ["Woraus wird eigentlich Weißwurst gemacht?", "Ich glaube, Weißwurst wird aus Kalbfleisch gemacht. Warum?", "Weil mir davon immer schlecht wird.", "Ach du Arme."],
 "Schweineohren (pl) | Blätterteig" : ["Woraus werden eigentlich Schweineohren gemacht?", "Ich glaube, Schweineohren werden aus Blätterteig gemacht. Warum?", "Weil mir davon immer schlecht wird.", "Ach du Arme."],
 "Schnitzel | Kalbfleisch" : ["Woraus werden eigentlich Schnitzel gemacht?","Ich glaube, Schnitzel werden aus Kalbfleisch gemacht. Warum?", "Weil mir davon immer schlecht wird.", "Ach du Arme."],
-"Gummibärchen (pl) | Zucker (m)" : ["Woraus werden eigentlich Gummibärchen gemacht?","Ich glaube, Gummibärchen werden aus Gelatine gemacht. Warum?", "Weil mir davon immer schlecht wird.", "Ach du Arme."],
+"Gummibärchen (pl) | Zucker (m)" : ["Woraus werden eigentlich Gummibärchen gemacht?","Ich glaube, Gummibärchen werden aus Zucker gemacht. Warum?", "Weil mir davon immer schlecht wird.", "Ach du Arme."],
 "Saumagen | Fleischreste" : ["Woraus wird eigentlich Saumagen gemacht?","Ich glaube, Saumagen wird aus Fleischresten gemacht. Warum?", "Weil mir davon immer schlecht wird.", "Ach du Arme."],
 "grüne Soße | frische Kräuter" : ["Woraus eigentlich grüne Soße gemacht?","Ich glaube, grüne Soße wird aus frischen Kräutern gemacht. Warum?", "Weil mir davon immer schlecht wird.", "Ach du Arme."],
 "Bier | Hopfen" : ["Woraus wird eigentlich Bier gemacht?", "Ich glaube, Bier wird aus Hopfen gemacht. Warum?", "Weil mir davon immer schlecht wird.", "Ach du Arme."]}
