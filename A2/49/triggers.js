0ed103d7dd2f2110162b9e8044d527f063c17f5b
smarterGerman <65780492+smarterGerman@users.noreply.github.com>
2023-07-16 15:19:05 +0200

    Tennis spielen.js

--- A2/49/triggers.js
+++ A2/49/triggers.js
@@ -6,9 +6,9 @@ let allTriggerAnswersJSON = `[
 "Ich gieße immer für ihn die Blumen." : ["Kennst du eigentlich Herrn Scholz?", "Unseren neuen Nachbarn aus dem ersten Stock?", "Genau, den meine ich.", "Das ist der, für den ich immer die Blumen gieße."],
 "Ich bringe immer für sie die Pakete zur Post." : ["Kennst du eigentlich Frau Kohl?", "Unsere neue Nachbarin aus dem ersten Stock?", "Genau, die meine ich.", "Das ist die, für die ich immer die Pakete zur Post bringe."],
 "Ich gehe immer mit ihm Kaffee trinken." : ["Kennst du eigentlich Herrn Scholz?", "Unseren neuen Nachbarn aus dem ersten Stock?", "Genau, den meine ich.", "Das ist der, mit dem ich immer Kaffee trinken gehe."],
-"Ich gehe immer mit ihr Tennis spielen." : ["Kennst du eigentlich Frau Kohl?", "Unsere neue Nachbarin aus dem ersten Stock?", "Genau, die meine ich.", "Das ist die, mit der ich immer Tennis spiele."],
+"Ich spiele immer mit ihr Tennis." : ["Kennst du eigentlich Frau Kohl?", "Unsere neue Nachbarin aus dem ersten Stock?", "Genau, die meine ich.", "Das ist die, mit der ich immer Tennis spiele."],
 "Ich kenne ihn vom Yoga." : ["Kennst du eigentlich Herrn Scholz?", "Unseren neuen Nachbarn aus dem ersten Stock?", "Genau, den meine ich.", "Das ist der, den ich vom Yoga kenne."],
 "Ich kenne sie aus dem Club." : ["Kennst du eigentlich Frau Kohl?", "Unsere neue Nachbarin aus dem ersten Stock?", "Genau, die meine ich.", "Das ist die, die ich aus dem Club kenne."]}
 ]`;
 
-const triggers = ['Er putzt immer die Fenster.', 'Sie trennt immer sorgfältig den Müll.', 'Er hört immer klassische Musik.', 'Sie spielt immer Klarinette.', 'Ich gieße immer für ihn die Blumen.', 'Ich bringe immer für sie die Pakete zur Post.', 'Ich gehe immer mit ihm Kaffee trinken.', 'Ich gehe immer mit ihr Tennis spielen.', 'Ich kenne ihn vom Yoga.', 'Ich kenne sie aus dem Club.'];
+const triggers = ['Er putzt immer die Fenster.', 'Sie trennt immer sorgfältig den Müll.', 'Er hört immer klassische Musik.', 'Sie spielt immer Klarinette.', 'Ich gieße immer für ihn die Blumen.', 'Ich bringe immer für sie die Pakete zur Post.', 'Ich gehe immer mit ihm Kaffee trinken.', 'Ich spiele immer mit ihr Tennis.', 'Ich kenne ihn vom Yoga.', 'Ich kenne sie aus dem Club.'];
