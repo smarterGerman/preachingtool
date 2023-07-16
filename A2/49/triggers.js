80c44dc06d14c641487fc4d3ff6eab83894c5c29
smarterGerman <65780492+smarterGerman@users.noreply.github.com>
2023-07-16 15:11:55 +0200

    Update Klarinette.js

--- A2/49/triggers.js
+++ A2/49/triggers.js
@@ -2,7 +2,7 @@ let allTriggerAnswersJSON = `[
 {"Er putzt immer die Fenster." : ["Kennst du eigentlich Herrn Scholz?", "Unseren neuen Nachbarn aus dem ersten Stock?", "Genau, den meine ich.", "Das ist der, der immer die Fenster putzt."],
 "Sie trennt immer sorgfältig den Müll." : ["Kennst du eigentlich Frau Kohl?", "Unsere neue Nachbarin aus dem ersten Stock?", "Genau, die meine ich.", "Das ist die, die immer sorgfältig den Müll trennt."],
 "Er hört immer klassische Musik." : ["Kennst du eigentlich Herrn Scholz?", "Unseren neuen Nachbarn aus dem ersten Stock?", "Genau, den meine ich.", "Das ist der, der immer klassische Musik hört."],
-"Sie spielt immer täglich Klarinette." : ["Kennst du eigentlich Frau Kohl?", "Unsere neue Nachbarin aus dem ersten Stock?", "Genau, die meine ich.", "Das ist die, die immer Klarinette spielt."],
+"Sie spielt immer Klarinette." : ["Kennst du eigentlich Frau Kohl?", "Unsere neue Nachbarin aus dem ersten Stock?", "Genau, die meine ich.", "Das ist die, die immer Klarinette spielt."],
 "Ich gieße immer für ihn die Blumen." : ["Kennst du eigentlich Herrn Scholz?", "Unseren neuen Nachbarn aus dem ersten Stock?", "Genau, den meine ich.", "Das ist der, für den ich immer die Blumen gieße."],
 "Ich bringe immer für sie die Pakete zur Post." : ["Kennst du eigentlich Frau Kohl?", "Unsere neue Nachbarin aus dem ersten Stock?", "Genau, die meine ich.", "Das ist die, für die ich immer die Pakete zur Post bringe."],
 "Ich gehe immer mit ihm Kaffee trinken." : ["Kennst du eigentlich Herrn Scholz?", "Unseren neuen Nachbarn aus dem ersten Stock?", "Genau, den meine ich.", "Das ist der, mit dem ich immer Kaffee trinken gehe."],
