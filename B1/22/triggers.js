eac7fdf77f46e429f0cb84e433ce766bbaa51c88
smarterGerman <65780492+smarterGerman@users.noreply.github.com>
2023-07-18 18:40:28 +0200

    Update triggers schule renoviert full stop.js

--- B1/22/triggers.js
+++ B1/22/triggers.js
@@ -2,7 +2,7 @@ let allTriggerAnswersJSON = `[
 {"Redakteur| entlassen werden" : ["Ich habe gehört, dass der Redakteur entlassen wird.", "Das stimmt. Der Redakteur wird entlassen.", "Warum wird der Redakteur denn entlassen?", "Darüber möchte ich nicht sprechen."],
 "Schiller Institut | schließen" : ["Ich habe gehört, dass das Schiller Institut geschlossen wird.", "Das stimmt. Das Schiller Institut  wird geschlossen.", "Warum wird das Schiller Institut denn geschlossen?", "Darüber möchte ich nicht sprechen."],
 "Kirche | umbauen" : ["Ich habe gehört, dass die Kirche umgebaut wird.", "Das stimmt. Die Kirche wird umgebaut.", "Warum wird die Kirche denn umgebaut?", "Darüber möchte ich nicht sprechen."],
-"Schule | renoviert" : ["Ich habe gehört, dass die Schule renoviert wird.", "Das stimmt. Die Schule wird renoviert .", "Warum wird die Schule denn renoviert?", "Darüber möchte ich nicht sprechen."],
+"Schule | renoviert" : ["Ich habe gehört, dass die Schule renoviert wird.", "Das stimmt. Die Schule wird renoviert.", "Warum wird die Schule denn renoviert?", "Darüber möchte ich nicht sprechen."],
 "Zirkus | boykottieren" : ["Ich habe gehört, dass der Zirkus boykottiert wird.", "Das stimmt. Der Zirkus wird boykottiert.", "Warum wird der Zirkus denn boykottiert?", "Darüber möchte ich nicht sprechen."],
 "die Preise | erhöhen" : ["Ich habe gehört, dass die Preise erhöht werden.", "Das stimmt. Die Preise werden erhöht.", "Warum werden die Preise denn erhöht?", "Darüber möchte ich nicht sprechen."],
 "Denkmal | abreißen" : ["Ich habe gehört, dass das Denkmal abgerissen wird.", "Das stimmt. Das Denkmal wird abgerissen.", "Warum wird das Denkmal denn abgerissen?", "Darüber möchte ich nicht sprechen."],
