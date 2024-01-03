192d22932bf49c9c3897b62f1c8a988b17e8df64
smarterGerman <65780492+smarterGerman@users.noreply.github.com>
2024-01-03 21:38:25 +0100

    Update triggers.js

--- B1/26/triggers.js
+++ B1/26/triggers.js
@@ -1,8 +1,8 @@
 let allTriggerAnswersJSON = `[
 {"Freund - (m) | überfallen" : ["Was ist mit deinem Freund passiert?", "Er ist überfallen worden.", "Warum ist dein Freund überfallen worden?", "Weil er halt überfallen worden ist."],
-"Auto | waschen" : ["Was ist mit deinem Auto passiert?", "Sie ist angefahren worden.", "Warum ist deine Frau angefahren worden?", "Weil sie halt angefahren worden ist."],
+"Auto | waschen" : ["Was ist mit deinem Auto passiert?", "Es ist gewaschen worden.", "Warum ist dein Auto gewaschen worden?", "Weil es halt gewaschen worden ist."],
 "Tasche | stehlen" : ["Was ist mit deiner Tasche passiert?", "Sie ist gestohlen worden.", "Warum ist deine Tasche gestohlen worden?", "Weil sie halt gestohlen worden ist."],
-"Frau | anfahren" : ["Was ist mit deiner Frau passiert?", "Sie ist angefahren worden.", "Warum ist sie angefahren worden?", "Weil sie halt angefahren worden ist."],
+"Frau | anfahren" : ["Was ist mit deiner Frau passiert?", "Sie ist angefahren worden.", "Warum ist deine Frau angefahren worden?", "Weil sie halt angefahren worden ist."],
 "Sohn | entlassen" : ["Was ist mit deinem Sohn passiert?", "Er ist entlassen worden", "Warum ist dein Sohn entlassen worden?", "Weil er halt entlassen worden ist."],
 "Handy | zerkratzen" : ["Was ist mit deinem Handy passiert?", "Es ist zerkratzt worden.", "Warum ist dein Handy zerkratzt worden?", "Weil es halt zerkratzt worden ist."],
 "Wohnung | demolieren" : ["Was ist mit deiner Wohnung passiert?", "Sie ist demoliert worden.", "Warum ist deine Wohnung demoliert worden?", "Weil sie halt demoliert worden ist."],
