42e557df5d610e3ea52ebcabbc2ba1de17063422
smarterGerman <65780492+smarterGerman@users.noreply.github.com>
2024-02-01 20:25:57 +0100

    Update triggers.js

--- A2/40/triggers.js
+++ A2/40/triggers.js
@@ -1,5 +1,5 @@
 let allTriggerAnswersJSON = `[
-{"Kino | renovieren" : ["Hast du schon gehört? Das Metropolis wird geschlossen.", "Warum wird das Metropolis denn geschlossen?", "Es wird endlich renoviert.", "Das wurde aber auch Zeit!"],
+{"Kino | schließen" : ["Hast du schon gehört? Das Kino wird geschlossen.", "Warum wird das Kino denn geschlossen?", "Es wird endlich renoviert.", "Das wurde aber auch Zeit!"],
 "Handtaschenmuseum | restaurieren" : ["Hast du schon gehört? Das Handtaschenmuseum wird geschlossen.", "Warum wird das Handtaschenmuseum denn geschlossen?", "Es wird endlich restauriert.", "Das wurde aber auch Zeit!"],
 "Kindergarten | umbauen" : ["Hast du schon gehört? Der Kindergarten wird geschlossen.", "Warum wird der Kindergarten denn geschlossen?", "Er wird endlich umgebaut.", "Das wurde aber auch Zeit!"],
 "Stadtpark | vergrößern" : ["Hast du schon gehört? Der Stadtpark wird geschlossen.", "Warum wird der Stadtpark denn geschlossen?", "Er wird endlich vergrößert.", "Das wurde aber auch Zeit!"],
@@ -11,4 +11,4 @@ let allTriggerAnswersJSON = `[
 "Gemeindehaus | sanieren" : ["Hast du schon gehört? Das Gemeindehaus wird geschlossen.", "Warum wird das Gemeindehaus denn geschlossen?", "Es wird endlich saniert.", "Das wurde aber auch Zeit!"]}
 ]`;
 
-const triggers = ['Kino | renovieren', 'Handtaschenmuseum | restaurieren', 'Kindergarten | umbauen', 'Stadtpark | vergrößern', 'Wurstfabrik | abreißen', 'Flughafen | ausbauen', 'Musikschule | streichen', 'Tempelhof | neu bepflanzen', 'Zoo | renovieren', 'Gemeindehaus | sanieren'];
+const triggers = ['Kino | schließen', 'Handtaschenmuseum | restaurieren', 'Kindergarten | umbauen', 'Stadtpark | vergrößern', 'Wurstfabrik | abreißen', 'Flughafen | ausbauen', 'Musikschule | streichen', 'Tempelhof | neu bepflanzen', 'Zoo | renovieren', 'Gemeindehaus | sanieren'];
