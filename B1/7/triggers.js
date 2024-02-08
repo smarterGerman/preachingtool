6246cef3fccc3ab64777423bdd94a7307736e2e7
smarterGerman <65780492+smarterGerman@users.noreply.github.com>
2024-02-08 21:09:33 +0100

    Update triggers.js

--- B1/7/triggers.js
+++ B1/7/triggers.js
@@ -1,14 +1,14 @@
 let allTriggerAnswersJSON = `[
-{"Mann | sprechen mit + D" : ["Kennst du den Mann?", "Welchen Mann?", "Na den, mit dem ich spreche.", "Ja klar. Den kenne ich."],
+{"Mann | sprechen mit + D" : ["Siehst du den Mann?", "Welchen Mann?", "Na den, mit dem ich spreche.", "Ja klar. Den sehe ich."],
 "Geschenk | sich freuen über + A" : ["Siehst du das Geschenk?", "Welches Geschenk?", "Na das, über das ich mich freue.", "Ja klar. Das sehe ich."],
-"Frau | träumen von + D" : ["Kennst du die Frau?", "Welche Frau?", "Na die, von der ich träume.", "Ja klar. Die kenne ich."],
+"Frau | träumen von + D" : ["Siehst du die Frau?", "Welche Frau?", "Na die, von der ich träume.", "Ja klar. Die sehe ich."],
 "Priester | sich unterhalten mit + D" : ["Siehst du den Priester?", "Welchen Priester?", "Na den, mit dem ich mich unterhalte.", "Ja klar. Den sehe ich."],
-"Gott | glauben an + A" : ["Kennst du den Gott?", "Welchen Gott?", "Na den, an den ich glaube.", "Ja klar. Den kenne ich."],
+"Gott | glauben an + A" : ["Siehst du den Gott?", "Welchen Gott?", "Na den, an den ich glaube.", "Ja klar. Den sehe ich."],
 "Bus | warten auf + A" : ["Siehst du den Bus?", "Welchen Bus?", "Na den, auf den ich warte.", "Ja klar. Den sehe ich."],
 "Auto | träumen von + D" : ["Siehst du das Auto?", "Welches Auto?", "Na das, von dem ich träume", "Ja klar. Das sehe ich."],
-"Politiker | halten von + D" : ["Kennst du den Politiker?", "Welchen Politiker?", "Na den, von dem ich nichts halte", "Ja klar. Den kenne ich."],
+"Politiker | halten von + D" : ["Siehst du den Politiker?", "Welchen Politiker?", "Na den, von dem ich nichts halte", "Ja klar. Den sehe ich."],
 "Spinne | sich fürchten vor + D" : ["Siehst du die Spinne?", "Welche Spinne?", "Na die, vor der ich mich fürchte.", "Ja klar. Die sehe ich."],
-"Seminar | teilnehmen an + D" : ["Kennst du das Seminar?", "Welches Seminar?", "Na das, an dem ich teilnehme.", "Ja klar. Das kenne ich."]}
+"Seminar | teilnehmen an + D" : ["Siehst du das Seminar?", "Welches Seminar?", "Na das, an dem ich teilnehme.", "Ja klar. Das sehe ich."]}
 ]`;
 
 const triggers = ['Mann | sprechen mit + D', 'Geschenk | sich freuen über + A', 'Frau | träumen von + D', 'Priester | sich unterhalten mit + D', 'Gott | glauben an + A', 'Bus | warten auf + A', 'Auto | träumen von + D', 'Politiker | halten von + D', 'Spinne | sich fürchten vor + D', 'Seminar | teilnehmen an + D'];
