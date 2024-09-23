581fb7eb1637736bcc6f1104f109e9c14df58945
smarterGerman <65780492+smarterGerman@users.noreply.github.com>
2024-09-23 13:20:15 +0200

    multiple changes to all triggers and preaching B2L38

--- B2/38/triggers.js
+++ B2/38/triggers.js
@@ -1,12 +1,12 @@
 let allTriggerAnswersJSON = `[
-{"Er arbeitet hier. | angestellt" : ["Kennst du die Person dort?", "Er arbeitet hier.", "Aha, ein Angestellter", "Du hast es erfasst."],
-"Er ist mein Neffe | verwandt" : ["Kennst du die Person dort?", "Er ist mein Neffe.", "Aha, ein Verwandter.", "Du hast es erfasst."],
-"Er kommt aus Deutschland. | Deutsch" : ["Kennst du die Person dort?", "Er kommt aus Deutschland..", "Aha, ein Deutscher.", "Du hast es erfasst."],
-"Ich kenne ihn | vertraut" : ["Kennst du die Person dort?", "Er ist mir vertraut.", "Aha, ein Vertrauter.", "Du hast es erfasst."],
-"Ich liebe ihn | geliebt" : ["Kennst du die Person dort?", "Er liebt mich.", "Aha, ein Geliebter.", "Du hast es erfasst."],
-"Ich habe Angst vor ihm. | verrückt" : ["Kennst du die Person dort?", "Er hat Angst vor mir.", "Aha, ein Ängstlicher", "Du hast es erfasst."],
-"Nein. Aber er sieht hilfsbereit aus. | gut" : ["Kennst du die Person dort?", "Er ist neureich.", "Aha, ein Neureicher.", "Du hast es erfasst."],
-"Nein. Aber er fährt ein fettes Auto. | neureich" : ["Kennst du die Person dort?", "Er ist betrunken.", "Aha, ein Betrunkener.", "Du hast es erfasst."]}
+{"Er arbeitet hier. | angestellt" : ["Kennst du die Person dort?", "Er arbeitet hier.", "Aha, er ist ein Angestellter", "Du hast es erfasst."],
+"Sie ist meine Nichte. | verwandt" : ["Kennst du die Person dort?", "Sie ist meine Nichte.", "Aha, Aha, sie ist deine Verwandte.", "Du hast es erfasst."],
+"Er kommt aus Deutschland. | Deutsch" : ["Kennst du die Person dort?", "Er kommt aus Deutschland..", "Aha, er ist ein Deutscher.", "Du hast es erfasst."],
+"Ich kenne sie | vertraut" : ["Kennst du die Person dort?", "Ich kenne sie.", "Aha, sie ist eine Vertraute.", "Du hast es erfasst."],
+"Ich liebe ihn | geliebt" : ["Kennst du die Person dort?", "Ich liebe ihn.", "Aha, er ist ein Geliebter.", "Du hast es erfasst."],
+"Ich habe Angst vor ihr. | verrückt" : ["Kennst du die Person dort?", "Ich habe Angst vor ihr.", "Aha, sie ist eine Verrückte.", "Du hast es erfasst."],
+"Nein. Aber er sieht hilfsbereit aus. | gut" : ["Kennst du die Person dort?", "Nein, aber er sieht hilfsbereit aus.", "Aha, er ist ein Guter.", "Du hast es erfasst."],
+"Nein. Aber sie fährt ein fettes Auto. | neureich" : ["Kennst du die Person dort?", "Nein, aber sie fährt ein fettes Auto.", "Aha, sie ist eine Neureiche.", "Du hast es erfasst."]}
 ]`;
 
-const triggers = ['Er arbeitet hier. | angestellt', 'Er ist mein Neffe | verwandt', 'Er kommt aus Deutschland. | Deutsch', 'Ich kenne ihn | vertraut', 'Ich liebe ihn | geliebt', 'Ich habe Angst vor ihm. | verrückt', 'Nein. Aber er sieht hilfsbereit aus. | gut', 'Nein. Aber er fährt ein fettes Auto. | neureich'];
+const triggers = ['Er arbeitet hier. | angestellt', 'Sie ist meine Nichte. | verwandt', 'Er kommt aus Deutschland. | Deutsch', 'Ich kenne sie | vertraut', 'Ich liebe ihn | geliebt', 'Ich habe Angst vor ihr. | verrückt', 'Nein. Aber er sieht hilfsbereit aus. | gut', 'Nein. Aber sie fährt ein fettes Auto. | neureich'];
