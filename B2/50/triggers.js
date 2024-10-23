c4f3075302207901bca0c4e1cfc5aecf2f3db078
smarterGerman <65780492+smarterGerman@users.noreply.github.com>
2024-10-23 14:59:20 +0200

    B2L50 + audio

--- B2/50/triggers.js
+++ B2/50/triggers.js
@@ -1,2 +1,24 @@
-
-
+let allTriggerAnswersJSON = `[
+    {"Der Prophet kann nicht verstanden werden." : ["Man kann den Propheten nicht verstehen.","Der Prophet ist nicht zu verstehen.", "Der Prophet ist unverständlich.","Der Prophet lässt sich nicht verstehen."],
+    "Ihr Bein kann amputiert werden." : ["Man kann ihr Bein amputieren.","Das Bein ist zu amputieren.","Das Bein ist amputierbar.","Das Bein lässt sich amputieren."],
+    "Ein Passwort kann einfach geknackt werden." : ["Man kann das Passwort einfach knacken.","Das Passwort ist einfach zu knacken.","Das Passwort ist knackbar.","Das Passwort lässt sich einfach knacken."],
+    "Das Ziel kann nicht erreicht werden." : ["Man kann das Ziel nicht erreichen.","Das Ziel ist nicht zu erreichen.","Das Ziel ist unerreichbar.","Das Ziel lässt sich nicht erreichen."],
+    "Wir können nicht übertroffen werden." : ["Man kann uns nicht übertreffen.","Wir sind nicht zu übertreffen.","Wir sind unübertrefflich.","Wir lassen uns nicht übertreffen."],
+    "Der Urlaub auf Bali kann nicht vergessen werden." : ["Man kann den Urlaub auf Bali nicht vergessen.","Der Urlaub auf Bali ist nicht zu vergessen.","Der Urlaub auf Bali ist unvergesslich.","Der Urlaub auf Bali lässt sich nicht vergessen."],
+    "Das Geschirr kann nicht zerbrochen werden. " : ["Man kann das Geschirr nicht zerbrechen.","Das Geschirr ist nicht zu zerbrechen.","Das Geschirr ist unzerbrechlich.","Das Geschirr ist unzerbrechlich.
+Das Geschirr lässt sich nicht zerbrechen."],
+    "Die Mona Lisa kann nicht verkauft werden. " : ["Man kann die Mona Lisa nicht verkaufen.","Die Mona Lisa ist nicht zu verkaufen.","Die Mona Lisa ist unverkäuflich.","Die Mona Lisa lässt sich nicht verkaufen."],
+    "Viele Fehler können vermieden werden." : ["Man kann viele Fehler vermeiden.","Viele Fehler sind zu vermeiden.","Viele Fehler sind vermeidbar.","Viele Fehler lassen sich vermeiden."],
+    "Viele Ruinen können nicht betreten werden." : ["Man kann viele Ruinen nicht betreten.","Viele Ruinen sind nicht zu betreten.","Viele Ruinen sind unbetretbar.","Viele Ruinen lassen sich nicht betreten."]}
+    ]`;
+    
+    const triggers = ['Der Prophet kann nicht verstanden werden.', 
+        'Ihr Bein kann amputiert werden.',
+        'Ein Passwort kann einfach geknackt werden.',
+        'Das Ziel kann nicht erreicht werden.',
+        'Wir können nicht übertroffen werden.',
+        'Der Urlaub auf Bali kann nicht vergessen werden.',
+        'Das Geschirr kann nicht zerbrochen werden. ',
+        'Die Mona Lisa kann nicht verkauft werden. ',
+        'Viele Fehler können vermieden werden.',
+        'Viele Ruinen können nicht betreten werden.'];
