2ca8c252717391ef310f5d2eb23e4fed7020fa55
smarterGerman <65780492+smarterGerman@users.noreply.github.com>
2024-10-31 14:37:18 +0100

    B1L29 remove space in trigger

--- B1/29/triggers.js
+++ B1/29/triggers.js
@@ -4,7 +4,7 @@ let allTriggerAnswersJSON = `[
 "Das Paket kann nicht zugestellt werden." : ["Man kann das Paket nicht zustellen.", "Das Paket ist nicht zuzustellen.", "Das Paket ist unzustellbar.", "Das Paket lässt sich nicht zustellen."],
 "Die Datei kann nicht aufgefunden werden." : ["Man kann die Datei nicht auffinden.", "Die Datei ist nicht aufzufinden.", "Die Datei ist unauffindbar.", "Die Datei lässt sich nicht finden."],
 "Das Gesicht kann nicht erkannt werden." : ["Man kann das Gesicht nicht erkennen.", "Das Gesicht ist nicht zu erkennen.", "Das Gesicht ist unerkennbar.", "Das Gesicht lässt sich nicht erkennen."],
-"Das Auto kann nicht repariert werden. (-abel)" : ["Man kann das Auto nicht reparieren.", "Das Auto ist nicht zu reparieren.", "Das Auto ist irreparabel.", "Das Auto lässt sich nicht reparieren."],
+"Das Auto kann nicht repariert werden.(-abel)" : ["Man kann das Auto nicht reparieren.", "Das Auto ist nicht zu reparieren.", "Das Auto ist irreparabel.", "Das Auto lässt sich nicht reparieren."],
 "Der Täter kann nicht identifiziert werden" : ["Man kann den Täter nicht identifizieren.", "Der Täter ist nicht zu identifizieren.", "Der Täter ist unidentifizierbar.", "Der Täter lässt sich nicht identifizieren."],
 "Die Tür kann nicht ersetzt werden." : ["Man kann die Tür nicht ersetzen.", "Die Tür ist nicht zu ersetzen.", "Die Tür ist unersetzbar.", "Die Tür lässt sich nicht ersetzen."],
 "Die gewünschte Person kann nicht erreicht werden." : ["Man kann die gewünschte Person nicht erreichen.", "Die gewünschte Person ist nicht zu erreichen.", "Die gewünschte Person ist unerreichbar.", "Die gewünschte Person lässt sich nicht erreichen."],
