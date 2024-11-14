18d87f832d3b1b32beacab13bc3fb10f7d7a6194
smarterGerman <65780492+smarterGerman@users.noreply.github.com>
2024-11-14 15:35:51 +0100

    Update triggers.js

--- B1/40/triggers.js
+++ B1/40/triggers.js
@@ -2,7 +2,7 @@ let allTriggerAnswersJSON = `[
 {"Sie müssen den Chef nicht anrufen." : ["Sie brauchen den Chef nicht anzurufen.", "Wieso brauche ich den Chef nicht anzurufen?", "Weil Sie den Chef nicht anrufen brauchen.", "Ich möchte aber den Chef anrufen."],
 "Du musst das Auto nicht volltanken." : ["Du brauchst das Auto nicht vollzutanken.", "Wieso brauche ich das Auto nicht vollzutanken?", "Weil du das Auto nicht volltanken brauchst.", "Ich möchte aber das Auto volltanken."],
 "Sie müssen kein Russisch lernen." : ["Sie brauchen kein Russisch zu lernen.", "Wieso brauche ich kein Russisch zu lernen?", "Weil Sie kein Russisch lernen brauchen.", "Ich möchte aber Russisch lernen."],
-"Du musst heute nicht arbeiten." : ["Du brauchst heute nicht arbeiten.", "Wieso brauche ich heute nicht zu arbeiten?", "Weil du heute nicht arbeiten brauchst.", "Ich möchte heute aber arbeiten."],
+"Du musst heute nicht arbeiten." : ["Du brauchst heute nicht zu arbeiten.", "Wieso brauche ich heute nicht zu arbeiten?", "Weil du heute nicht zu arbeiten brauchst.", "Ich möchte heute aber arbeiten."],
 "Sie müssen die Schuhe nicht ausziehen." : ["Sie brauchen die Schuhe nicht auszuziehen.", "Wieso brauche ich die Schuhe nicht auszuziehen?", "Weil Sie die Schuhe nicht ausziehen brauchen.", "Ich möchte aber die Schuhe  ausziehen."],
 "Du musst kein Ticket kaufen." : ["Du brauchst kein Ticket zu kaufen.", "Wieso brauche ich kein Ticket zu kaufen?", "Weil du kein Ticket kaufen brauchst.", "Ich möchte aber ein Ticket kaufen."],
 "Sie müssen keinen neuen Computer kaufen." : ["Sie brauchen keinen neuen Computer zu kaufen.", "Wieso brauche ich keinen neuen Computer zu kaufen?", "Weil Sie keinen Computer kaufen brauchen.", "Ich möchte aber keinen Computer kaufen."],
