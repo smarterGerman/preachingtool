60eaa46c58e78e9f560fe52f6bc52f469d0a760b
smarterGerman <65780492+smarterGerman@users.noreply.github.com>
2025-06-10 21:17:39 +0200

    Update triggers.js
    removed a "nein"

--- B1/42/triggers.js
+++ B1/42/triggers.js
@@ -2,8 +2,8 @@ let allTriggerAnswersJSON = `[
 {"es mögen | einkaufen" : ["Kaufst du ein?", "Ich mag es, einzukaufen.", "Warum magst du es denn, einzukaufen?", "Weil ich es schlicht und einfach mag."],
 "es leid sein | alles organisieren" : ["Organisierst du alles?", "Ich bin es leid, alles zu organisieren.", "Warum bist du es denn leid, alles zu organisieren?", "Weil ich es schlicht und einfach leid bin."],
 "es ist nötig | aufräumen" : ["Räumst du auf?", "Es ist nötig, aufzuräumen.", "Warum ist es denn nötig, aufzuräumen?", "Weil es schlicht und einfach nötig ist."],
-"es hassen | kochen" : ["Kochst du?", "Ich hasse es, zu kochen.", "Warum hasst du es denn, zu kochen?", "Weil ich es schlicht und einfach hasse."],
-"es fühlt sich gut an | die Arbeit stehen lassen" : ["Lässt du die Arbeit stehen?", "Nein, es fühlt sich gut an, die Arbeit stehen zu lassen.", "Warum fühlt es sich denn gut an, die Arbeit stehen zu lassen?", "Weil es sich schlicht und einfach gut anfühlt."],
+"es lieben | kochen" : ["Kochst du?", "Ich liebe es, zu kochen.", "Warum liebst du es denn, zu kochen?", "Weil ich es schlicht und einfach liebe."],
+"es fühlt sich gut an | die Arbeit stehen lassen" : ["Lässt du die Arbeit stehen?", "Es fühlt sich gut an, die Arbeit stehen zu lassen.", "Warum fühlt es sich denn gut an, die Arbeit stehen zu lassen?", "Weil es sich schlicht und einfach gut anfühlt."],
 "es macht Spaß | putzen" : ["Putzt du?", "Es macht Spaß, zu putzen.", "Warum macht es denn Spaß, zu putzen?", "Weil es schlicht und einfach Spaß macht."],
 "es tut mir gut | rauchen" : ["Rauchst du?", "Es tut mir gut, zu rauchen.", "Warum tut es dir denn gut, zu rauchen?", "Weil es mir schlicht und einfach gut tut."],
 "es liegt mir | handwerklich arbeiten" : ["Arbeitest du handwerklich?", "Es liegt mir, handwerklich zu arbeiten.", "Warum liegt es dir denn, handwerklich zu arbeiten?", "Weil es mir schlicht und einfach liegt."],
@@ -11,4 +11,4 @@ let allTriggerAnswersJSON = `[
 "es ist selbstverständlich | alles alleine machen" : ["Machst du alles alleine?", "Es ist selbstverständlich, alles alleine zu machen.", "Warum ist es denn selbstverständlich, alles alleine zu machen?", "Weil es schlicht und einfach selbstverständlich ist."]}
 ]`;
 
-const triggers = ['es mögen | einkaufen', 'es leid sein | alles organisieren', 'es ist nötig | aufräumen', 'es hassen | kochen', 'es fühlt sich gut an | die Arbeit stehen lassen', 'es macht Spaß | putzen', 'es tut mir gut | rauchen', 'es liegt mir  | handwerklich arbeiten', 'es ist langweilig | für die Prüfung lernen', 'es ist selbstverständlich | alles alleine machen'];
+const triggers = ['es mögen | einkaufen', 'es leid sein | alles organisieren', 'es ist nötig | aufräumen', 'es lieben | kochen', 'es fühlt sich gut an | die Arbeit stehen lassen', 'es macht Spaß | putzen', 'es tut mir gut | rauchen', 'es liegt mir  | handwerklich arbeiten', 'es ist langweilig | für die Prüfung lernen', 'es ist selbstverständlich | alles alleine machen'];
