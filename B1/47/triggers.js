a973f59f6bb8d10c593a67b9b25bb7b092696f9d
smarterGerman <65780492+smarterGerman@users.noreply.github.com>
2025-06-13 19:42:34 +0200

    Update triggers.js
    friseur = frisör

--- B1/47/triggers.js
+++ B1/47/triggers.js
@@ -6,9 +6,9 @@ let allTriggerAnswersJSON = `[
 "ihr | nicht von der Arbeit kommen" : ["Wir kommen nicht von der Arbeit.", "Das glaub ich nicht.", "Ihr seht aus, als ob ihr von der Arbeit kommt.", "Ihr seht aus, als ob ihr von der Arbeit kommen würdet."],
 "du | kein Bier brauchen" : ["Ich brauche kein Bier.", "Das glaub ich nicht.", "Du siehst aus, als ob du ein Bier brauchst.", "Du siehst aus, als ob du ein Bier brauchen würdest."],
 "Sie | nichts/etwas im Schilde führen" : ["Ich führe nichts im Schilde.", "Das glaub ich nicht.", "Sie sehen aus, als ob Sie etwas im Schilde führen.", "Sie sehen aus, als ob Sie etwas im Schilde führen würden."],
-"sein Friseur | nicht im Urlaub sein" : ["Sein Frisör ist nicht im Urlaub.", "Das glaub ich nicht.", "Er sieht aus, als ob sein Friseur im Urlaub ist.", "Er sieht aus, als ob sein Friseur im Urlaub wäre."],
+"sein Frisör | nicht im Urlaub sein" : ["Sein Frisör ist nicht im Urlaub.", "Das glaub ich nicht.", "Er sieht aus, als ob sein Frisör im Urlaub ist.", "Er sieht aus, als ob sein Frisör im Urlaub wäre."],
 "ihr | nicht gerne Fußball spielen" : ["Wir spielen nicht gerne Fußball.", "Das glaub ich nicht.", "Ihr seht aus, als ob ihr gerne Fußball spielt.", "Ihr seht aus, als ob ihr gerne Fußball spielen würdet."],
 "Sie | nicht gleich in Ohnmacht fallen" : ["Ich falle nicht gleich in Ohnmacht.", "Das glaub ich nicht.", "Sie sehen aus, als ob Sie gleich in Ohnmacht fallen.", "Sie sehen aus, als ob Sie gleich in Ohnmacht fallen würden."]}
 ]`;
 
-const triggers = ['er | nicht müde sein', 'sie | kein Deutsch verstehen', 'du | nichts/etwas sagen wollen', 'sie | keine Polizistin sein', 'ihr | nicht von der Arbeit kommen', 'du | kein Bier brauchen', 'Sie | nichts/etwas im Schilde führen', 'sein Friseur | nicht im Urlaub sein', 'ihr | nicht gerne Fußball spielen', 'Sie | nicht gleich in Ohnmacht fallen'];
+const triggers = ['er | nicht müde sein', 'sie | kein Deutsch verstehen', 'du | nichts/etwas sagen wollen', 'sie | keine Polizistin sein', 'ihr | nicht von der Arbeit kommen', 'du | kein Bier brauchen', 'Sie | nichts/etwas im Schilde führen', 'sein Frisör | nicht im Urlaub sein', 'ihr | nicht gerne Fußball spielen', 'Sie | nicht gleich in Ohnmacht fallen'];
