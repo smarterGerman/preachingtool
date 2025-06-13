f1358986e825920740946b7cbd6f122371b2d6cd
smarterGerman <65780492+smarterGerman@users.noreply.github.com>
2025-06-13 19:31:37 +0200

    Update triggers.js
    consistency improvements

--- B1/47/triggers.js
+++ B1/47/triggers.js
@@ -1,14 +1,14 @@
 let allTriggerAnswersJSON = `[
-{"er | sein müde" : ["Er ist nicht müde.", "Das glaub ich nicht.", "Er sieht aus, als ob er müde ist.", "Er sieht aus, als ob er müde wäre."],
-"sie | verstehen Deutsch" : ["Sie versteht kein Deutsch.", "Das glaub ich nicht.", "Sie sieht aus, als ob sie Deutsch versteht.", "Sie sieht aus, als ob sie Deutsch verstehen würde."],
-"du | wollen etwas sagen" : ["Ich will nichts sagen.", "Das glaub ich nicht.", "Du siehst aus, als ob du etwas willst.", "Du siehst aus, als ob du etwas sagen wolltest."],
-"sie | sein Polizistin" : ["Sie ist keine Polizistin.", "Das glaub ich nicht.", "Sie sieht aus, als ob sie Polizistin ist.", "Sie sieht aus, als ob sie Polizistin wäre."],
-"ihr | kommen von der Arbeit" : ["Wir kommen nicht von der Arbeit.", "Das glaub ich nicht.", "Ihr seht aus, als ob ihr von der Arbeit kommt.", "Ihr seht aus, als ob ihr von der Arbeit kommen würdet."],
-"du | brauchen ein Bier" : ["Ich brauche kein Bier.", "Das glaub ich nicht.", "Du siehst aus, als ob du ein Bier brauchst.", "Du siehst aus, als ob du ein Bier brauchen würdest."],
-"Sie | führen etwas im Schilde" : ["Ich führe nichts im Schilde.", "Das glaub ich nicht.", "Sie sehen aus, als ob Sie etwas im Schilde führen.", "Sie sehen aus, als ob Sie etwas im Schilde führen würden."],
-"sein Friseur | sein in Urlaub" : ["Sein Frisör ist nicht im Urlaub.", "Das glaub ich nicht.", "Er sieht aus, als ob sein Friseur im Urlaub ist.", "Er sieht aus, als ob sein Friseur im Urlaub wäre."],
-"ihr | spielen gerne Fußball" : ["Wir spielen nicht gerne Fußball.", "Das glaub ich nicht.", "Ihr seht aus, als ob ihr gerne Fußball spielt.", "Ihr seht aus, als ob ihr gerne Fußball spielen würdet."],
-"Sie | fallen gleich in Ohnmacht" : ["Ich falle nicht gleich in Ohnmacht.", "Das glaub ich nicht.", "Sie sehen aus, als ob Sie gleich in Ohnmacht fallen.", "Sie sehen aus, als ob Sie gleich in Ohnmacht fallen würden."]}
+{"er | nicht müde sein" : ["Er ist nicht müde.", "Das glaub ich nicht.", "Er sieht aus, als ob er müde ist.", "Er sieht aus, als ob er müde wäre."],
+"sie | kein Deutsch verstehen" : ["Sie versteht kein Deutsch.", "Das glaub ich nicht.", "Sie sieht aus, als ob sie Deutsch versteht.", "Sie sieht aus, als ob sie Deutsch verstehen würde."],
+"du | nichts/etwas sagen wollen" : ["Ich will nichts sagen.", "Das glaub ich nicht.", "Du siehst aus, als ob du etwas willst.", "Du siehst aus, als ob du etwas sagen wolltest."],
+"sie | keine Polizistin sein" : ["Sie ist keine Polizistin.", "Das glaub ich nicht.", "Sie sieht aus, als ob sie Polizistin ist.", "Sie sieht aus, als ob sie Polizistin wäre."],
+"ihr | nicht von der Arbeit kommen" : ["Wir kommen nicht von der Arbeit.", "Das glaub ich nicht.", "Ihr seht aus, als ob ihr von der Arbeit kommt.", "Ihr seht aus, als ob ihr von der Arbeit kommen würdet."],
+"du | kein Bier brauchen" : ["Ich brauche kein Bier.", "Das glaub ich nicht.", "Du siehst aus, als ob du ein Bier brauchst.", "Du siehst aus, als ob du ein Bier brauchen würdest."],
+"Sie | nichts/etwas im Schilde führen" : ["Ich führe nichts im Schilde.", "Das glaub ich nicht.", "Sie sehen aus, als ob Sie etwas im Schilde führen.", "Sie sehen aus, als ob Sie etwas im Schilde führen würden."],
+"sein Friseur | nicht im Urlaub sein" : ["Sein Frisör ist nicht im Urlaub.", "Das glaub ich nicht.", "Er sieht aus, als ob sein Friseur im Urlaub ist.", "Er sieht aus, als ob sein Friseur im Urlaub wäre."],
+"ihr | nicht gerne Fußball spielen" : ["Wir spielen nicht gerne Fußball.", "Das glaub ich nicht.", "Ihr seht aus, als ob ihr gerne Fußball spielt.", "Ihr seht aus, als ob ihr gerne Fußball spielen würdet."],
+"Sie | nicht gleich in Ohnmacht fallen" : ["Ich falle nicht gleich in Ohnmacht.", "Das glaub ich nicht.", "Sie sehen aus, als ob Sie gleich in Ohnmacht fallen.", "Sie sehen aus, als ob Sie gleich in Ohnmacht fallen würden."]}
 ]`;
 
-const triggers = ['er | sein müde', 'sie | verstehen Deutsch', 'du | wollen etwas sagen', 'sie | sein Polizistin', 'ihr | kommen von der Arbeit', 'du | brauchen ein Bier', 'Sie | führen etwas im Schilde', 'sein Friseur | sein in Urlaub', 'ihr | spielen gerne Fußball', 'Sie | fallen gleich in Ohnmacht'];
+const triggers = ['er | nicht müde sein', 'sie | kein Deutsch verstehen', 'du | nichts/etwas sagen wollen', 'sie | keine Polizistin sein', 'ihr | nicht von der Arbeit kommen', 'du | kein Bier brauchen', 'Sie | nichts/etwas im Schilde führen', 'sein Friseur | nicht im Urlaub sein', 'ihr | nicht gerne Fußball spielen', 'Sie | nicht gleich in Ohnmacht fallen'];
