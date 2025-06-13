49efdd9a9c1cdec37f09b2460991cb01f323c3ff
smarterGerman <65780492+smarterGerman@users.noreply.github.com>
2025-06-13 20:01:09 +0200

    Update triggers.js
    wolltest to wollen würdest

--- B1/47/triggers.js
+++ B1/47/triggers.js
@@ -1,7 +1,7 @@
 let allTriggerAnswersJSON = `[
 {"er | nicht müde sein" : ["Er ist nicht müde.", "Das glaub ich nicht.", "Er sieht aus, als ob er müde ist.", "Er sieht aus, als ob er müde wäre."],
 "sie | kein Deutsch verstehen" : ["Sie versteht kein Deutsch.", "Das glaub ich nicht.", "Sie sieht aus, als ob sie Deutsch versteht.", "Sie sieht aus, als ob sie Deutsch verstehen würde."],
-"du | nichts/etwas sagen wollen" : ["Ich will nichts sagen.", "Das glaub ich nicht.", "Du siehst aus, als ob du etwas sagen willst.", "Du siehst aus, als ob du etwas sagen wolltest."],
+"du | nichts/etwas sagen wollen" : ["Ich will nichts sagen.", "Das glaub ich nicht.", "Du siehst aus, als ob du etwas sagen willst.", "Du siehst aus, als ob du etwas sagen wollen würdest."],
 "sie | keine Polizistin sein" : ["Sie ist keine Polizistin.", "Das glaub ich nicht.", "Sie sieht aus, als ob sie Polizistin ist.", "Sie sieht aus, als ob sie Polizistin wäre."],
 "ihr | nicht von der Arbeit kommen" : ["Wir kommen nicht von der Arbeit.", "Das glaub ich nicht.", "Ihr seht aus, als ob ihr von der Arbeit kommt.", "Ihr seht aus, als ob ihr von der Arbeit kommen würdet."],
 "du | kein Bier brauchen" : ["Ich brauche kein Bier.", "Das glaub ich nicht.", "Du siehst aus, als ob du ein Bier brauchst.", "Du siehst aus, als ob du ein Bier brauchen würdest."],
